4e98ebf378e988b4fc05252896e7be5cac74583d
Jason Bock <jason@jasonbock.net>
2017-06-11 19:37:02 -0400

    Rx examples

---
+++ ReactiveSignalR.Client/ObservableNativeKeyWatcher.cs
@@ -0,0 +1,32 @@
+﻿using KeyWatching;
+using System;
+using System.Collections.Generic;
+using System.Reactive.Disposables;
+
+namespace ReactiveSignalR.Client
+{
+	internal sealed class ObservableNativeKeyWatcher
+		: NativeKeyWatcher, IObservable<char>
+	{
+		private readonly List<IObserver<char>> observers =
+			new List<IObserver<char>>();
+
+		public IDisposable Subscribe(IObserver<char> observer)
+		{
+			if(!this.observers.Contains(observer))
+			{
+				this.observers.Add(observer);
+			}
+
+			return Disposable.Empty;
+		}
+
+		protected override void HandleKey(int keyCode)
+		{
+			foreach(var observer in this.observers)
+			{
+				observer.OnNext(Convert.ToChar(keyCode));
+			}
+		}
+	}
+}
